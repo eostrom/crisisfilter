00c89441695e6c6b0e5417dd64e004a686fcedaf
Erik Ostrom <erik@echographia.com>
2010-01-16 22:40:31 -0800

    Up the log level so Heroku will log it.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -62,7 +62,7 @@ class ReportsController < ApplicationController
     params[:search][:created_at_gte] ||= 1.hour.ago
     params[:search][:descend_by_votes] ||= true
 
-    logger.debug("SEARCH #{params[:search].inspect}")
+    logger.error("SEARCH #{params[:search].inspect}")
     @search = Report.search(params[:search])
     @reports = @search.paginate(:page => params[:page])
   end
