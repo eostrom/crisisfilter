f7c33b090ff3a976e15fa9ec4c39c60fcb975344
alain <alainbloch@gmail.com>
2010-01-29 16:33:03 -0800

    removed upvote and downvote from reports controller and moved the logic to the new votes controller.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,5 @@
 class ReportsController < ApplicationController
 
-  before_filter :find_report, :except => [:index]
-
-  REPORTS_PER_PAGE = 20 # should be moved into model
-
   def index
     refreshed = Report.refresh_if_needed
     flash.now[:refresh] = "#{refreshed} new tweets" if refreshed
@@ -16,37 +12,12 @@ class ReportsController < ApplicationController
     # Explicit declaration of searched fields so that users can't hack the search    
     @search  = Report.search(:content_like => params[:search][:content_like],
                              :order        => params[:search][:order])
-    @reports = @search.paginate(:page => params[:page],
-                                :per_page => REPORTS_PER_PAGE)
+    @reports = @search.paginate(:page => params[:page])
                                 
     respond_to do |format|
       format.html
       format.xml  { render :xml => @reports }
     end
-  end
-
-  def upvote
-    @report.increment!(:upvotes)
-    message = "upvoted: #{@report.content}"
-    respond_to do |format|
-      format.html { flash[:notice] = message; redirect_to reports_path }
-      format.js   { render :action => "update" } #update.js.rjs
-    end
-  end
-
-  def downvote
-    @report.increment!(:downvotes)
-    message = "downvoted: #{@report.content}"
-    respond_to do |format|
-      format.html { flash[:notice] = message; redirect_to reports_path }
-      format.js   { render :action => "update" } #update.js.rjs
-    end
-  end
-
-  private
-
-  def find_report
-    @report = Report.find(params[:id])
   end
 
 end
