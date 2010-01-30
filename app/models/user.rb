b153c1c622a2bbafdcfdfc4815a61df417ee8693
alain <alainbloch@gmail.com>
2010-01-29 17:04:22 -0800

    added report and vote associations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :downvotes
   has_many :upvotes
   
+  has_many :reports, :through => :votes
+  has_many :upvoted_reports,   :through => :upvotes,   :source => :report
+  has_many :downvoted_reports, :through => :downvotes, :source => :report
+  
   # Overriding authlogic validations
   acts_as_authentic do |auth|
     auth.require_password_confirmation = false
