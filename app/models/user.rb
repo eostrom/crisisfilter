c28e8c2d43f4be25cd02864b1510de53d8d5efcf
alain <alainbloch@gmail.com>
2010-01-29 16:54:17 -0800

    fixed associations in vote and user models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   
+  has_many :votes
+  has_many :downvotes
+  has_many :upvotes
+  
   # Overriding authlogic validations
   acts_as_authentic do |auth|
     auth.require_password_confirmation = false
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,6 @@
 class Vote < ActiveRecord::Base
   
-  has_one :user
-  has_one :report
+  belongs_to :user
   
   validates_presence_of :user, :report
   validates_uniqueness_of :user_id, :scope => [:report_id], :message => "has already voted on this report."
